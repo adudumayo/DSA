508a005b1df1cc82040a7e8fe5d535609613658f
Asimanye Dudumayo <adudumayo023@student.wethinkcode.co.za>
2024-04-18 19:37:01 +0200

    added the add methods

--- linked_lists/IntegerList.java
+++ linked_lists/IntegerList.java
@@ -1,5 +1,3 @@
-package DSA.linked_lists;
-
 public class IntegerList {
     private class ListNode {
         public int data;
@@ -8,4 +6,43 @@ public class IntegerList {
 
     private ListNode first;
     private int numElements;
+
+    public IntegerList() {
+        first = null;
+        numElements = 0;
+    } // basic constructor with an empty list
+
+    public void add(int item) 
+    // this adds a node at the beginning of the linkedList
+    {
+        ListNode node = new ListNode();
+        node.data = item;
+        node.next = first;
+
+        first = node;
+        numElements++;
+    } // add
+
+    public void add(int item, int position)
+    // this adds a node at a given position, and adds at the end as default
+    {
+        ListNode node = new ListNode();
+        node.data = item;
+        ListNode frontLink = first;
+        ListNode backLink = null;
+
+        for (int i = 0; i < position && frontLink != null; i++) {
+            backLink = frontLink;
+            frontLink = frontLink.next;
+        }
+
+        node.next = frontLink;
+
+        if (backLink != null) {
+            backLink.next = node;
+        } else {
+            first = node;
+        }
+    } // add with item and position
+
 } // InterList Class

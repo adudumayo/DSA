b45441eab6a6c07dc0b24377fabaeb366a199199
Asimanye Dudumayo <adudumayo023@student.wethinkcode.co.za>
2024-04-18 18:55:17 +0200

    Put in the instance variables and inner class

--- linked_lists/IntegerList.java
+++ linked_lists/IntegerList.java
@@ -1,5 +1,11 @@
 package DSA.linked_lists;
 
 public class IntegerList {
-    
-}
+    private class ListNode {
+        public int data;
+        public ListNode next;
+    }
+
+    private ListNode first;
+    private int numElements;
+} // InterList Class

612f7d63d1c9f75d029c8ff3f6250bcb5bafbb08
Asimanye Dudumayo <148616095+adudumayo@users.noreply.github.com>
2024-04-19 23:17:40 +0200

    Add files via upload
    because I have issues with git

---
+++ linked_lists/ObjectList.java
@@ -0,0 +1,156 @@
+public class ObjectList {
+    private class ListNode {
+        public Object data;
+        public ListNode next;
+    }
+
+    private ListNode first;
+    private int numElements;
+
+    public ObjectList() {
+        first = null;
+        numElements = 0;
+    } // basic constructor with an empty list
+
+    public void add(Object item)
+    // this adds a node at the beginning of the linkedList
+    {
+        ListNode node = new ListNode();
+        node.data = item;
+        node.next = first;
+
+        first = node;
+        numElements++;
+    } // add
+
+    public void add(Object item, int position)
+    // this adds a node at a given position, and adds at the end as default
+    {
+        ListNode node = new ListNode();
+        node.data = item;
+        ListNode frontLink = first;
+        ListNode backLink = null;
+
+        for (int i = 0; i < position && frontLink != null; i++) {
+            backLink = frontLink;
+            frontLink = frontLink.next;
+        }
+
+        node.next = frontLink;
+
+        if (backLink != null) {
+            backLink.next = node;
+        } else {
+            first = node;
+        }
+
+        numElements++;
+    } // add with item and position
+
+    public Object get(int index) {
+        if (index < 0 || index > numElements) {
+            throw new IndexOutOfBoundsException("Your index is too big or negative");
+        }
+
+        ListNode temp = first;
+
+        for (int i = 0; i < index && temp != null; i++) {
+            temp = temp.next;
+        }
+
+        assert temp != null;
+
+        return temp.data;
+    } // getter
+
+    public void set(int index, Object item) {
+        if (index < 0 || index > numElements) {
+            throw new IndexOutOfBoundsException("Your index is either negative or too big");
+        }
+
+        ListNode temp = first;
+
+        for (int i = 0; i < index && temp != null; i++) {
+            temp = temp.next;
+        }
+
+        assert temp != null;
+
+        temp.data = item;
+    } // setter
+
+    public int length() {
+        return this.numElements;
+    } // returns the length of the list
+
+    public int position(Object item)
+    // find the position of a given item
+    {
+        ListNode temp = first;
+
+        int index;
+
+        for (index = 0; temp != null && !temp.data.equals(item); index++, temp = temp.next) {
+            ;
+        }
+
+        if (temp == null) {
+            return -1;
+        } else {
+            return index;
+        }
+    } // position
+
+    // I did not test this one, Ich bin sehr mude
+    // Ich denke it must be index instead of position
+    public void remove(int position) {
+        if (position < 0 || position >= numElements) {
+            throw new IndexOutOfBoundsException("The position is out of range");
+        }
+
+        ListNode frontLink = first, backLink = null;
+
+        for (int i = 0; i < position && frontLink != null; i++) {
+            backLink = frontLink;
+            frontLink = frontLink.next;
+        }
+
+        assert frontLink != null;
+
+        if (backLink != null) {
+            backLink.next = frontLink.next;
+        } else {
+            first = frontLink.next;
+        }
+        numElements--;
+    } // remove
+
+    public String toString() {
+        StringBuffer s = new StringBuffer("[");
+        for (ListNode curr = first; curr != null; curr = curr.next) {
+            s.append("" + curr.data);
+            if (curr.next != null)
+                s.append(", ");
+        }
+        s.append("]");
+        return s.toString();
+    } // toString
+
+    public static void main(String[] args) {
+        IntegerList firstList = new IntegerList();
+        for (int i = 0; i <= 10; i++) {
+            firstList.add(i, i);
+        }
+
+        System.out.println(firstList);
+        System.out.println(firstList.get(3));
+        firstList.set(3, 30);
+        System.out.println(firstList);
+        System.out.println(firstList.length());
+        firstList.remove(10);
+        System.out.println(firstList);
+        System.out.println(firstList.position(9));
+
+    }
+
+} // ObjectList Class
